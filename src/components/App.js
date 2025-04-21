b8a848e266424c6bf884aea71e4fbdddd4669364
Tanishka Chaudhary <chaudharytanishka38@gmail.com>
2025-04-21 21:33:41 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React,{useState}from "react";
 import './../styles/App.css';
+import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
+
+  let [weatherData,setweatherData] = useState({ temperature: 25, conditions: "Sunny" })
   return (
     <div>
-        {/* Do not remove the main div */}
+        <WeatherDisplay
+         temperature={weatherData.temperature}
+         conditions={weatherData.conditions}
+        />
     </div>
   )
 }
---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+
+const WeatherDisplay = (props) =>{
+
+const tempStyle = {
+
+    color : props.temperature>20 ? 'red' : 'blue'
+}
+    return(
+
+        <div>
+            <p>
+                Temperature : <span style={tempStyle}>{props.temperature}</span>
+            </p>
+            <p>Condition : {props.conditions}</p>
+        </div>
+    )
+
+
+}
+
+export default WeatherDisplay;
